a46b8597537cb39f8c22ce2c45fd874b7bf8be58
andres02-art <a.arregoces35@gmail.com>
2023-03-02 18:44:36 -0500

    Version 0.11
    se a corregiodo un error en las migraciones que impedia realizar las
    tablas

--- database/migrations/2023_03_02_213901_create_books_table.php
+++ database/migrations/2023_03_02_213901_create_books_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('title');
             $table->integer('stock');
             $table->text('description');
-            $table->foreignId('authors_id');
             $table->timestamps();
         });
     }
