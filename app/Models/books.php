a2b66e53cb902febe5768b900eb74858d0df5fe3
andres02-art <a.arregoces35@gmail.com>
2023-03-02 19:29:14 -0500

    Version 0.0.03
    -se han añadido llaves foranes en la tabla categories
    -se ha corregido un error que impedia crear las migraciones por el orden
    el que se daba

--- app/Models/books.php
+++ app/Models/books.php
@@ -1,11 +1,24 @@
 <?php
 
 namespace App\Models;
+namespace App\Models\books;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Authors;
+use App\Models\Authors\Authors as AuthorsAuthors;
+use App\Models\categories;
+use App\Models\categories\categories as CategoriesCategories;
 
 class books extends Model
 {
     use HasFactory;
+    public function AutBoo()
+    {
+        return $this->hasMany(AuthorsAuthors::class);
+    }
+    public function catBoo()
+    {
+        return $this->hasMany(CategoriesCategories::class);
+    }
 }
